75575f8027a9e47dd11a3dfad0cf60d1b5a5df9e
Everton Braga <evertonr.braga@gmail.com>
2019-03-04 23:09:33 -0300

    jpa mysql config

---
+++ src/main/java/com/fatec/lab5/Aluno.java
@@ -0,0 +1,56 @@
+package com.fatec.lab5;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Aluno {
+
+	String nome;
+	int idade;
+	int semestre;
+	String ra;
+	char sexo;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public int getSemestre() {
+		return semestre;
+	}
+	
+	public void setSemestre(int semestre) {
+		this.semestre = semestre;
+	}
+	
+	public String getRa() {
+		return ra;
+	}
+	
+	public void setRa(String ra) {
+		this.ra = ra;
+	}
+	
+	public char getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	
+}
--- src/main/java/com/fatec/lab5/Lab5Application.java
+++ src/main/java/com/fatec/lab5/Lab5Application.java
@@ -2,12 +2,17 @@ package com.fatec.lab5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Lab5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Lab5Application.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(Lab5Application.class, args);
+		Aluno aluno01 = applicationContext.getBean(Aluno.class);
+		aluno01.setNome("Carlos");
+		System.out.println(aluno01.getNome());
+		
 	}
 
 }
