5a3a4714c702560fd24c454b86df12df89f7315b
Everton Braga <evertonr.braga@gmail.com>
2019-03-08 15:57:00 -0300

    implementation class Curso

--- src/main/java/br/gov/sp/fatec/Application.java
+++ src/main/java/br/gov/sp/fatec/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import br.gov.sp.fatec.model.Aluno;
+import br.gov.sp.fatec.model.Curso;
 
 @SpringBootApplication
 public class Application {
@@ -14,6 +15,10 @@ public class Application {
 		Aluno aluno01 = applicationContext.getBean(Aluno.class);
 		aluno01.setNome("Carlos");
 		System.out.println(aluno01.getNome());
+		
+		Curso curso01 = applicationContext.getBean(Curso.class);
+		curso01.setNome("Banco de Dados");
+		System.out.println(curso01.getNome());
 		
 	}
 
