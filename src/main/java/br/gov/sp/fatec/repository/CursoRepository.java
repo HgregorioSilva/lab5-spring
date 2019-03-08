0d0a7f8e666b584f4f685e31040aa11cb2921b73
Everton Braga <evertonr.braga@gmail.com>
2019-03-07 23:55:55 -0300

    query curso

--- src/main/java/br/gov/sp/fatec/repository/CursoRepository.java
+++ src/main/java/br/gov/sp/fatec/repository/CursoRepository.java
@@ -1,5 +1,8 @@
 package br.gov.sp.fatec.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.gov.sp.fatec.model.Curso;
@@ -7,5 +10,8 @@ import br.gov.sp.fatec.model.Curso;
 public interface CursoRepository extends CrudRepository<Curso, Long> {
 
 	public Curso findByNome(String nome);
+	
+	@Query("select u from Usuario u where u.nome like %?1%")
+	public List<Curso> buscaCurso(String nome);
 	
 }
