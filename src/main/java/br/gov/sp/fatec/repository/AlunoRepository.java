e2a5fb2efb8a0bf1b8808a88fdbcb1813fbce9df
Everton Braga <evertonr.braga@gmail.com>
2019-03-07 23:57:59 -0300

    query Aluno

--- src/main/java/br/gov/sp/fatec/repository/AlunoRepository.java
+++ src/main/java/br/gov/sp/fatec/repository/AlunoRepository.java
@@ -1,11 +1,18 @@
 package br.gov.sp.fatec.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import br.gov.sp.fatec.model.Aluno;
 import br.gov.sp.fatec.model.Curso;
 
 public interface AlunoRepository extends CrudRepository<Curso, Long> {
 
 	public Curso findByNome(String nome);
+	
+	@Query("select a from Aluno a where a.nome like %?1%")
+	public List<Aluno> buscaAluno(String nome);
 	
 }
--- src/main/java/br/gov/sp/fatec/repository/CursoRepository.java
+++ src/main/java/br/gov/sp/fatec/repository/CursoRepository.java
@@ -11,7 +11,7 @@ public interface CursoRepository extends CrudRepository<Curso, Long> {
 
 	public Curso findByNome(String nome);
 	
-	@Query("select u from Curso u where u.nome like %?1%")
+	@Query("select c from Curso c where c.nome like %?1%")
 	public List<Curso> buscaCurso(String nome);
 	
 }
